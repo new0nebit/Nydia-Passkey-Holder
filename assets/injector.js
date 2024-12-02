d1523295d6ec8805477b134d6efb88f12b723577
new0ne <new0nebit@gmail.com>
2024-12-02 05:12:54 +0200

    injector: remove js version in favor of typescript

--- assets/injector.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('Nydia.js');
-    script.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(script);
-})();
