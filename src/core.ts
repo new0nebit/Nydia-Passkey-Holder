9425ddfa10965c83608af058eed85a063dd7cbfb
new0ne <new0nebit@gmail.com>
2025-04-02 05:59:37 +0300

    chore: remove popup.ts import

--- src/core.ts
+++ src/core.ts
@@ -2,12 +2,8 @@ import './authenticator';
 import './algorithms';
 import './store';
 import './types';
-import './popup';
 import './logger';
-
 import { initializeAuthenticator } from './authenticator';
 
-/**
- * Initializes the WebAuthn authenticator when the core script is loaded.
- */
+// Initializes the WebAuthn authenticator when the core script is loaded.
 initializeAuthenticator();
