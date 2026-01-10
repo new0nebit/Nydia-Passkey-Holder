7a46b386da433ae00bcfa216b76b14d044efaebe
new0ne <new0nebit@gmail.com>
2026-01-10 21:40:24 +0200

    refactor(types): add WebAuthn option/response types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,29 @@
 export type WebAuthnOperationType = 'create' | 'get';
 
+export type BinaryLike = ArrayBuffer | ArrayBufferView;
+export type BinaryOrBase64 = BinaryLike | string;
+
+export type PublicKeyCredentialCreationOptions =
+  globalThis.PublicKeyCredentialCreationOptions;
+export type PublicKeyCredentialRequestOptions =
+  globalThis.PublicKeyCredentialRequestOptions;
+
+export interface CredentialCreationOptions {
+  publicKey: Omit<PublicKeyCredentialCreationOptions, 'challenge' | 'user'> & {
+    challenge: BinaryLike;
+    user: Omit<PublicKeyCredentialUserEntity, 'id'> & { id: BinaryOrBase64 };
+    rpId?: string;
+  };
+  origin: string;
+}
+
+export interface GetAssertionOptions {
+  publicKey: Omit<PublicKeyCredentialRequestOptions, 'challenge'> & {
+    challenge: BinaryOrBase64;
+  };
+  origin: string;
+}
+
 export interface StoredCredential {
   uniqueId: string;
   credentialId: string;
@@ -36,4 +60,29 @@ export interface EncryptedRecord {
   iv: string;
   data: string;
   isSynced: boolean;
+}
+
+export interface AttestationResponse {
+  type: 'public-key';
+  id: string;
+  rawId: string;
+  response: {
+    clientDataJSON: string;
+    attestationObject: string;
+    authenticatorData: string;
+    publicKeyAlgorithm: number;
+    publicKeyDER: string;
+  };
+}
+
+export interface AssertionResponse {
+  type: 'public-key';
+  id: string;
+  rawId: string;
+  response: {
+    clientDataJSON: string;
+    authenticatorData: string;
+    signature: string;
+    userHandle: string | null;
+  };
 }
