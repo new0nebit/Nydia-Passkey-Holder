3f503bebc610ccc1a5bd3bd7d7c7027c2b1a5683
new0ne <new0nebit@gmail.com>
2026-01-13 03:51:13 +0200

    feat(popup): redesign UI with modular SVG icons
    Refreshes the popup with the new design.

---
+++ src/ui/icons/popup/index.ts
@@ -0,0 +1,13 @@
+import globe from './globe.svg';
+import user from './user.svg';
+import lock from './lock.svg';
+import check from './check.svg';
+import spinner from './spinner.svg';
+
+export const icons = {
+  globe,
+  user,
+  lock,
+  check,
+  spinner,
+};
